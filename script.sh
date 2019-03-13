c467bf1b0092928119beeea632288d2f7c3b074f
educacionit <educacionit@localhost.localdomain>
2019-03-12 21:14:28 -0300

    Comentario

--- script.sh
+++ script.sh
@@ -1,7 +1,36 @@
 #!/bin/bash
 
+
+# BIENVENIDA
 echo "***********"
 echo "Hola World!"
 echo "***********"
+echo ""
 echo "Bienvenido al ejemplo de GIT HUB"
-echo "MODIFICACION 2"
+echo ""
+
+Pregunta()
+{
+    echo "Desea continuar? Y/N"
+    read respuesta
+
+    if { $respuesta -eq Y} 
+    then
+        echo "Usted ha elegido la opcion Y"
+        else
+        echo "Usted ha tomado el camino equivocado"
+        exit 0
+}
+
+# VARIABLES:
+
+# INPUT:
+
+# PROCESOS:
+
+    # Elegir camino:
+    Pregunta
+
+
+# OUTPUT
+
