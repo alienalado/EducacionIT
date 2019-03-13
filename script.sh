5e36524d20446d07bf8818145170e94f7f9c5977
educacionit <educacionit@localhost.localdomain>
2019-03-12 21:17:58 -0300

    Nuevo Update

--- script.sh
+++ script.sh
@@ -14,12 +14,13 @@ Pregunta()
     echo "Desea continuar? Y/N"
     read respuesta
 
-    if { $respuesta -eq Y} 
+    if [[ "$respuesta" = "Y" ]];
     then
         echo "Usted ha elegido la opcion Y"
-        else
+    else
         echo "Usted ha tomado el camino equivocado"
         exit 0
+    fi
 }
 
 # VARIABLES:
